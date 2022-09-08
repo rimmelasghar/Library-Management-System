0daa4501e2e9b2838afb0dc02a7af0a59990e115
rimmelasghar <rimmelasghar3@gmail.com>
2022-09-08 15:41:26 +0500

    addedloader

--- library/settings.py
+++ library/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,9 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'library/static/')
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
